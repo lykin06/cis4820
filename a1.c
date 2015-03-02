11de93c0518da414013e03ef322807909e60a655
Mathieu Boutelier <mbouteli@mail.uoguelph.ca>
2015-03-02 13:56:37 -0500

    included needed files for a3

--- a1.c
+++ a1.c
@@ -9,9 +9,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <math.h>
 #include <time.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "perlin.h"
 #include "graphics.h"
